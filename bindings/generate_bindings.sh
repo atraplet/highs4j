7f23144ff20570297bcfedad57f4d084e3e0ca7a
Adrian Trapletti <a.trapletti@ustermetrics.com>
2024-12-06 16:02:34 +0100

    Fix indentation

--- bindings/generate_bindings.sh
+++ bindings/generate_bindings.sh
@@ -6,12 +6,12 @@ Usage: generate_bindings path_to_highs_built_source_package path_to_jextract_bin
 
 # read command line arguments
 if [ $# -eq 2 ]; then
-    HIGHS4J=$(dirname "${0}")/../
-    HIGHS="${1}"
-    JEXTRACT="${2}"
+  HIGHS4J=$(dirname "${0}")/../
+  HIGHS="${1}"
+  JEXTRACT="${2}"
 else
-    echo "$USAGE"
-    exit 1
+  echo "$USAGE"
+  exit 1
 fi
 
 # remove old bindings
@@ -19,8 +19,8 @@ rm -rf "${HIGHS4J}"/src/main/java/com/ustermetrics/highs4j/bindings/
 
 # generate bindings
 $JEXTRACT \
-    --include-dir "${HIGHS}"/src \
-    --include-dir "${HIGHS}"/build \
-    --target-package com.ustermetrics.highs4j.bindings \
-    --output "${HIGHS4J}"/src/main/java \
-    @"${HIGHS4J}"/bindings/includes.txt "${HIGHS}"/src/interfaces/highs_c_api.h
+  --include-dir "${HIGHS}"/src \
+  --include-dir "${HIGHS}"/build \
+  --target-package com.ustermetrics.highs4j.bindings \
+  --output "${HIGHS4J}"/src/main/java \
+  @"${HIGHS4J}"/bindings/includes.txt "${HIGHS}"/src/interfaces/highs_c_api.h
--- bindings/generate_includes.sh
+++ bindings/generate_includes.sh
@@ -6,12 +6,12 @@ Usage: generate_includes path_to_highs_built_source_package path_to_jextract_bin
 
 # read command line arguments
 if [ $# -eq 2 ]; then
-    HIGHS4J=$(dirname "${0}")/../
-    HIGHS="${1}"
-    JEXTRACT="${2}"
+  HIGHS4J=$(dirname "${0}")/../
+  HIGHS="${1}"
+  JEXTRACT="${2}"
 else
-    echo "$USAGE"
-    exit 1
+  echo "$USAGE"
+  exit 1
 fi
 
 # define variables
@@ -22,10 +22,10 @@ INCLUDES="${HIGHS4J}"/bindings/includes.txt
 rm -f "${TMP_INCLUDES}"
 rm -f "${INCLUDES}"
 ${JEXTRACT} \
-    --include-dir "${HIGHS}"/src \
-    --include-dir "${HIGHS}"/build \
-    --dump-includes "${TMP_INCLUDES}" \
-    "${HIGHS}"/src/interfaces/highs_c_api.h
+  --include-dir "${HIGHS}"/src \
+  --include-dir "${HIGHS}"/build \
+  --dump-includes "${TMP_INCLUDES}" \
+  "${HIGHS}"/src/interfaces/highs_c_api.h
 
 # select symbols
 grep "highs" "${TMP_INCLUDES}" >"${INCLUDES}"
