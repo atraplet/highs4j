09e89ca8e5b84107221d171f49438b63831912fb
Adrian Trapletti <a.trapletti@ustermetrics.com>
2025-01-02 12:26:03 +0100

    Finalize test for Status

--- src/test/java/com/ustermetrics/highs4j/StatusTest.java
+++ src/test/java/com/ustermetrics/highs4j/StatusTest.java
@@ -1,11 +1,25 @@
 package com.ustermetrics.highs4j;
 
-import org.junit.jupiter.api.Test;
+import lombok.val;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class StatusTest {
 
-    @Test
-    void shouldTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})
+    void valueOfThrowsNoException(int status) {
+        assertDoesNotThrow(() -> Status.valueOf(status));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 100})
+    void valueOfThrowsException(int status) {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () -> Status.valueOf(status));
+
+        assertEquals("Unknown status " + status, exception.getMessage());
     }
 
 }
