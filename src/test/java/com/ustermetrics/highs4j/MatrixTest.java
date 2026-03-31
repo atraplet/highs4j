8fb37e5aa8ae8e8aa74ea512ecc1c4bd1de37a5f
Adrian Trapletti <a.trapletti@ustermetrics.com>
2026-03-31 13:26:32 +0200

    Add matrix test

---
+++ src/test/java/com/ustermetrics/highs4j/MatrixTest.java
@@ -0,0 +1,179 @@
+package com.ustermetrics.highs4j;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @Test
+    void createColwiseMatrixReturnsMatrix() {
+        val matrix = new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 1, 2}, new long[]{0, 1},
+                new double[]{6., 4.});
+
+        assertEquals(2, matrix.numRow());
+        assertEquals(2, matrix.numCol());
+        assertEquals(MatrixFormat.COLWISE, matrix.matrixFormat());
+        assertArrayEquals(new long[]{0, 1, 2}, matrix.start());
+        assertArrayEquals(new long[]{0, 1}, matrix.index());
+        assertArrayEquals(new double[]{6., 4.}, matrix.value(), 1e-8);
+    }
+
+    @Test
+    void createRowwiseMatrixReturnsMatrix() {
+        val matrix = new Matrix(2, 2, MatrixFormat.ROWWISE, new long[]{0, 1, 2}, new long[]{0, 1},
+                new double[]{6., 4.});
+
+        assertEquals(2, matrix.numRow());
+        assertEquals(2, matrix.numCol());
+        assertEquals(MatrixFormat.ROWWISE, matrix.matrixFormat());
+        assertArrayEquals(new long[]{0, 1, 2}, matrix.start());
+        assertArrayEquals(new long[]{0, 1}, matrix.index());
+        assertArrayEquals(new double[]{6., 4.}, matrix.value(), 1e-8);
+    }
+
+    @Test
+    void createZeroMatrixReturnsZeroMatrix() {
+        val matrix = new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 0, 0}, new long[]{}, new double[]{});
+
+        assertEquals(2, matrix.numRow());
+        assertEquals(2, matrix.numCol());
+        assertArrayEquals(new long[]{0, 0, 0}, matrix.start());
+        assertEquals(0, matrix.index().length);
+        assertEquals(0, matrix.value().length);
+    }
+
+    @Test
+    void createMatrixWithZeroNumberOfRowsThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(0, 0, MatrixFormat.COLWISE, new long[]{}, new long[]{}, new double[]{})
+        );
+
+        assertEquals("number of rows must be positive", exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithZeroNumberOfColumnsThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(1, 0, MatrixFormat.COLWISE, new long[]{}, new long[]{}, new double[]{})
+        );
+
+        assertEquals("number of columns must be positive", exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithZeroLengthColumnIndexThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(1, 1, MatrixFormat.COLWISE, new long[]{}, new long[]{}, new double[]{})
+        );
+
+        assertEquals("length of the column index must be positive", exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithInvalidDataThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 1, 2}, new long[]{0, 1},
+                        new double[]{6., 4., 5.})
+        );
+
+        assertEquals("length of data must be equal to the length of the row index", exception.getMessage());
+    }
+
+    @Test
+    void createColwiseMatrixWithInvalidColumnIndexThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 1, 2, 3}, new long[]{0, 1},
+                        new double[]{6., 4.})
+        );
+
+        assertEquals("length of the column index must be equal to the number of columns or rows plus one",
+                exception.getMessage());
+    }
+
+    @Test
+    void createRowwiseMatrixWithInvalidColumnIndexThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.ROWWISE, new long[]{0, 1, 2, 3, 4}, new long[]{0, 1},
+                        new double[]{6., 4.})
+        );
+
+        assertEquals("length of the column index must be equal to the number of columns or rows plus one",
+                exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithInvalidNumberOfNonZeroEntriesThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 1, 2}, new long[]{0, 1, 2, 3, 4},
+                        new double[]{0., 1., 2., 3., 4.})
+        );
+
+        assertEquals("number of non-zero entries must be less equal than the number of rows times the number of " +
+                "columns", exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithInvalidRowIndexThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 1, 2}, new long[]{-1, 1},
+                        new double[]{6., 4.})
+        );
+
+        assertEquals("entries of the row index must be greater equal zero and less than the number of rows or columns",
+                exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithInvalidColumnIndexEntriesThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{-1, 1, 2}, new long[]{0, 1},
+                        new double[]{6., 4.})
+        );
+
+        assertEquals("the first entry of the column index must be equal to zero and the last entry must be equal to " +
+                "the number of non-zero entries", exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithTooLargeEntryInColumnIndexThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(2, 2, MatrixFormat.COLWISE, new long[]{0, 3, 2}, new long[]{0, 1},
+                        new double[]{6., 4.})
+        );
+
+        assertEquals("entries of the column index must be greater equal zero, less equal than the number of non-zero " +
+                "entries, and must be ordered", exception.getMessage());
+    }
+
+    @Test
+    void createMatrixWithUnorderedRowIndexWithinColumnThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () ->
+                new Matrix(5, 4, MatrixFormat.COLWISE, new long[]{0, 2, 5, 7, 8}, new long[]{0, 3, 1, 4, 3, 0, 4, 4},
+                        new double[]{1., 4., 3., 5., 6., 2., 7., 8.})
+        );
+
+        assertEquals("entries of the row index within each column must be strictly ordered", exception.getMessage());
+    }
+
+    @Test
+    void createLargeMatrixDoesNotThrowException() {
+        val m = 46341;
+        val start = new long[m + 1];
+        Arrays.fill(start, 1, m + 1, 1);
+        assertDoesNotThrow(() ->
+                new Matrix(m, m, MatrixFormat.COLWISE, start, new long[]{0}, new double[]{1.})
+        );
+    }
+
+    @Test
+    void createMatrixWithNullMatrixFormatThrowsException() {
+        assertThrows(NullPointerException.class, () ->
+                new Matrix(2, 2, null, new long[]{0, 0, 0}, new long[]{}, new double[]{})
+        );
+    }
+
+}
