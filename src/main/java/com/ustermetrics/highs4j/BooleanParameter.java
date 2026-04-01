d7a0fd2169a711b1be09023915defe7a32cc904c
Adrian Trapletti <a.trapletti@ustermetrics.com>
2026-04-01 17:09:09 +0200

    Improve documentation and error message

--- src/main/java/com/ustermetrics/highs4j/BooleanParameter.java
+++ src/main/java/com/ustermetrics/highs4j/BooleanParameter.java
@@ -5,7 +5,7 @@ import lombok.NonNull;
 import static com.google.common.base.Preconditions.checkArgument;
 
 /**
- * A boolean <a href="https://highs.dev">HiGHS</a> solver option parameter.
+ * A parameter object for boolean <a href="https://highs.dev">HiGHS</a> solver options.
  *
  * @param name  the option name
  * @param value the option value
@@ -14,7 +14,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 public record BooleanParameter(@NonNull String name, boolean value) implements Parameter {
 
     public BooleanParameter {
-        checkArgument(!name.isBlank());
+        checkArgument(!name.isBlank(), "name cannot be blank");
     }
 
 }
