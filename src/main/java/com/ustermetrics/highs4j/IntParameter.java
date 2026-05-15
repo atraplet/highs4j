4152fa8c5eef77d9a2087b9574a5d2b6a49c442c
Adrian Trapletti <a.trapletti@ustermetrics.com>
2026-05-15 13:29:00 +0200

    Improve doc and error message

--- src/main/java/com/ustermetrics/highs4j/IntParameter.java
+++ src/main/java/com/ustermetrics/highs4j/IntParameter.java
@@ -5,7 +5,7 @@ import lombok.NonNull;
 import static com.google.common.base.Preconditions.checkArgument;
 
 /**
- * An integer <a href="https://highs.dev">HiGHS</a> solver option parameter.
+ * A parameter object for <a href="https://highs.dev">HiGHS</a> solver integer options.
  *
  * @param name  the option name
  * @param value the option value
@@ -14,7 +14,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 public record IntParameter(@NonNull String name, int value) implements Parameter {
 
     public IntParameter {
-        checkArgument(!name.isBlank());
+        checkArgument(!name.isBlank(), "name cannot be blank");
     }
 
 }
