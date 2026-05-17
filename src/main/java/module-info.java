d8a1a783f4016fcc5dfcc143494d3ba0f5c716e6
Adrian Trapletti <a.trapletti@ustermetrics.com>
2026-05-17 17:19:54 +0200

    Move slf4j dependency and update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.ustermetrics.highs4j {
     requires static lombok;
     requires com.google.common;
     requires org.scijava.nativelib;
+    requires org.slf4j;
 
     exports com.ustermetrics.highs4j;
 }
